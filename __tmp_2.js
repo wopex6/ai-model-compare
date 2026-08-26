ddbbd9e26262cbd08080b9e37d3c9b24311852a1
wopex6 <wopex6@yahoo.com>
2026-08-26 20:35:02 +1000

    Update frontend

--- __tmp_2.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/static/dr_health_sw.js')
-                .then((r) => console.log('Dr Health SW registered:', r.scope))
-                .catch((e) => console.log('Dr Health SW registration failed:', e));
-        }
